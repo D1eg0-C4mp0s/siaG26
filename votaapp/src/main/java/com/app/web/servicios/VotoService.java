46fdef1f0ba87356b8579912fff31c940262ea28
estiven <Estivenrendon1@outlook.es>
2022-12-10 13:19:15 -0500

    archivo war

--- votaapp/src/main/java/com/app/web/servicios/VotoService.java
+++ votaapp/src/main/java/com/app/web/servicios/VotoService.java
@@ -1,7 +1,6 @@
 package com.app.web.servicios;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.app.web.entidad.Voto;
@@ -11,10 +10,5 @@ public interface VotoService extends JpaRepository<Voto, Integer> {
 
 	
 	Voto findByIdentificacion(int identificacion);
-	
-	//@Query("SELECT COUNT(idenCandidato) FROM voto")
-	//int totalVotos();
-	
-	
 	
 }
